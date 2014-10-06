fed4600a63f41e8ada1499892aa498a8ae9c4b2e
Mark Petrovic <mark.petrovic@xoom.com>
2014-10-06 05:20:31 -0700

    Add non maven job listing

--- stashkins.go
+++ stashkins.go
@@ -10,9 +10,11 @@ import (
 import "github.com/xoom/jenkins"
 
 var (
-	stashURL                       = flag.String("stash-url", "http://stash.example.com", "Stash Base URL")
-	jenkinsBaseURL                 = flag.String("jenkins-url", "http://jenkins.example.com", "Jenkins Base URL")
-	listJobsWithoutFeatureBranches = flag.Bool("jobs-without-feature-branches", true, "List jobs without feature branches")
+	stashURL                            = flag.String("stash-url", "http://stash.example.com", "Stash Base URL")
+	jenkinsBaseURL                      = flag.String("jenkins-url", "http://jenkins.example.com", "Jenkins Base URL")
+	listJobsWithoutFeatureBranches      = flag.Bool("jobs-without-feature-branches", false, "List jobs without feature branches")
+	listJobsWithWildcardFeatureBranches = flag.Bool("jobs-with-wildcard-feature-branches", false, "List jobs with wildcard feature branches")
+	listNonMavenJobs                    = flag.Bool("non-maven-jobs", false, "List non-maven jobs")
 )
 
 func init() {
@@ -20,22 +22,38 @@ func init() {
 }
 
 func main() {
-	jobs, err := jenkins.GetJobs(*jenkinsBaseURL)
-	if err != nil {
-		log.Fatalf("GetJobs Error: %v\n", err)
+
+	if *listJobsWithWildcardFeatureBranches {
+		jobs, err := jenkins.GetJobs(*jenkinsBaseURL)
+		if err != nil {
+			log.Fatalf("GetJobs Error: %v\n", err)
+		}
+
+		for _, v := range jobs {
+			jobConfig, err := jenkins.GetJobConfig(*jenkinsBaseURL, v.Name)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %v, skipping...\n", v.Name, err)
+			}
+			for _, branch := range jobConfig.SCM.Branches.Branch {
+				if strings.Contains(branch.Name, "feature") && strings.Contains(branch.Name, "*") {
+					fmt.Printf("%s has branch wildcards: %s\n", v.URL, branch.Name)
+				}
+			}
+
+		}
 	}
 
-	for _, v := range jobs {
-		//fmt.Printf("%+v\n", v)
-		jobConfig, err := jenkins.GetJobConfig(*jenkinsBaseURL, v.Name)
+	if *listNonMavenJobs {
+		jobs, err := jenkins.GetJobs(*jenkinsBaseURL)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: %v, skipping...\n", v.Name, err)
-		}
-		for _, branch := range jobConfig.SCM.Branches.Branch {
-			if strings.Contains(branch.Name, "feature") && strings.Contains(branch.Name, "*") {
-				fmt.Printf("%s has branch wildcards: %s\n", v.URL, branch.Name)
-			}
+			log.Fatalf("GetJobs Error: %v\n", err)
 		}
 
+		for _, v := range jobs {
+			_, err := jenkins.GetJobConfig(*jenkinsBaseURL, v.Name)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Non-maven2 job: %s\n", v.Name)
+			}
+		}
 	}
 }
