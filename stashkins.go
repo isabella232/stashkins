a592e36686360690ed76ee4bcc7d92b9173c7e06
Mark Petrovic <mark.petrovic@xoom.com>
2014-10-07 05:26:54 -0700

    fold in obsolete job detection

--- stashkins.go
+++ stashkins.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 import (
 	"github.com/xoom/jenkins"
@@ -34,41 +35,34 @@ func main() {
 	}
 
 	if *jobReport {
-		log.Printf("Analyzing %s...\n", *jobRepositoryURL)
+		fmt.Fprintf(os.Stderr, "Analyzing %s...\n", *jobRepositoryURL)
 
-		// Jenkins
-		appConfigs := make([]jenkins.JobConfig, 0)
+		// Jenkins jobs which build against a branch under the Git URL
+		appJobConfigs := make([]jenkins.JobConfig, 0)
 		for _, job := range allJobs {
 			jobConfig, err := jenkins.GetJobConfig(*jenkinsBaseURL, job.Name)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "%s: %v, skipping...\n", job.Name, err)
 			}
 			for _, remoteCfg := range jobConfig.SCM.UserRemoteConfigs.UserRemoteConfig {
+				if strings.HasPrefix(remoteCfg.URL, "http") {
+					fmt.Fprintf(os.Stderr, "Found a job Git http URL.  This is not supported: %s\n", remoteCfg.URL)
+				}
 				fmt.Fprintf(os.Stderr, "checking url %s\n", remoteCfg.URL)
 				if remoteCfg.URL == *jobRepositoryURL {
-					appConfigs = append(appConfigs, jobConfig)
+					appJobConfigs = append(appJobConfigs, jobConfig)
 				}
 			}
 		}
-		branches := make([]jenkins.Branch, 0)
-		for _, v := range appConfigs {
-			for _, branch := range v.SCM.Branches.Branch {
-				branches = append(branches, branch)
-			}
-		}
-		fmt.Printf("Jenkins builds\n")
-		for _, v := range branches {
-			fmt.Printf("	%s\n", v.Name)
-		}
 
-		// Stash
+		// Get Stash branches for this repository.
 		repos, err := stash.GetRepositories(*stashBaseURL)
 		if err != nil {
 			log.Fatalf("Cannot get Stash repositories")
 		}
 		repo, ok := stash.HasRepository(repos, *jobRepositoryURL)
 		if !ok {
-			log.Fatalf("Unknown repository: %s\n", *jobRepositoryURL)
+			log.Fatalf("Unknown repository: %s\n", *jobRepositoryURL) // delete all entries of appJobConfigs - there is no repo here?
 		}
 
 		stashBranches, err := stash.GetBranches(*stashBaseURL, *stashUserName, *stashPassword, repo.Project.Key, repo.Slug)
@@ -80,5 +74,22 @@ func main() {
 		for _, v := range stashBranches {
 			fmt.Printf("	%s\n", v.DisplayID)
 		}
+
+		// Find branches Jenkins is building that no longer exist in Stash
+		obsoleteJobs := make([]jenkins.JobConfig, 0)
+		for _, jobConfig := range appJobConfigs {
+			deleteJobConfig := true
+			for _, builtBranch := range jobConfig.SCM.Branches.Branch {
+				for stashBranch, _ := range stashBranches {
+					if strings.HasSuffix(builtBranch.Name, stashBranch) {
+						deleteJobConfig = false
+					}
+				}
+			}
+			if deleteJobConfig {
+				obsoleteJobs = append(obsoleteJobs, jobConfig)
+			}
+		}
+		fmt.Printf("Obsolete jobs: %+v\n", obsoleteJobs)
 	}
 }
