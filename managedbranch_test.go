ee21b925da02f0e46885160b7feecf5e7f632283
Mark Petrovic <mark.petrovic@xoom.com>
2015-01-16 16:56:51 -0800

    INF-1254
    Uppercase "Test" in branch is managed test.  It was not running
    with "testXXX()"

--- managedbranch_test.go
+++ managedbranch_test.go
@@ -2,14 +2,18 @@ package main
 
 import "testing"
 
-func testBranchIsManaged(t *testing.T) {
-	if !branchIsManaged("master") {
+func TestBranchIsManaged(t *testing.T) {
+	if branchIsManaged("master") {
 		t.Fatalf("want false but got true\n")
 	}
-	if !branchIsManaged("develop") {
+	if branchIsManaged("develop") {
 		t.Fatalf("want false but got true\n")
 	}
-	if branchIsManaged("feature/somebranch") {
+
+	if !branchIsManaged("feature/somebranch") {
+		t.Fatalf("want true but got false\n")
+	}
+	if !branchIsManaged("origin/feature/somebranch") {
 		t.Fatalf("want true but got false\n")
 	}
 }
