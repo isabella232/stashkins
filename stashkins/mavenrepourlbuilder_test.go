bf5a75e5b6c181c5ce74a949c70adcdb145e85df
Mark Petrovic <mark.petrovic@xoom.com>
2015-03-18 18:00:21 -0700

    port another unit test to new package

--- stashkins/mavenrepourlbuilder_test.go
+++ stashkins/mavenrepourlbuilder_test.go
@@ -1,16 +1,18 @@
-package main
+package stashkins
 
 import "testing"
 
 func TestMavenRepoUrlBuilder(t *testing.T) {
+	s := StatelessOperations{}
+
 	want := "http://localhost:9090/nexus/content/repositories/PRJ.APP.feature_1"
-	url := buildMavenRepositoryURL("http://localhost:9090/nexus", "PRJ", "APP", "feature/1")
+	url := s.buildMavenRepositoryURL("http://localhost:9090/nexus", "PRJ", "APP", "feature/1")
 	if url != want {
 		t.Fatalf("Want %s but got %s\n", want, url)
 	}
 
 	want = "http://localhost:9090/nexus/content/repositories/snapshots"
-	url = buildMavenRepositoryURL("http://localhost:9090/nexus", "PRJ", "APP", "develop")
+	url = s.buildMavenRepositoryURL("http://localhost:9090/nexus", "PRJ", "APP", "develop")
 	if url != "http://localhost:9090/nexus/content/repositories/snapshots" {
 		t.Fatalf("Want %s but got %s\n", want, url)
 	}
