46cbb0fb2bafc7345928b3637b3c4086b7db7456
Mark Petrovic <mark.petrovic@xoom.com>
2015-03-18 17:43:17 -0700

    port another unit test

--- stashkins/isjobobsolete_test.go
+++ stashkins/isjobobsolete_test.go
@@ -16,38 +16,20 @@ func TestJobNotObsolete(t *testing.T) {
 	}
 }
 
-/*
 func TestJobNotObsoleteBranchUnmanaged(t *testing.T) {
-	builtBranches := []jenkins.Branch{
-		jenkins.Branch{Name: "origin/master"},
-	}
-	jobConfig := jenkins.JobConfig{SCM: jenkins.Scm{Branches: jenkins.Branches{Branch: builtBranches}}}
+	s := StatelessOperations{}
+	jobSummary := jenkins.JobSummary{Branch: "origin/master"}
 	stashBranches := map[string]stash.Branch{"feature/1": stash.Branch{}}
-	if shouldDeleteJob(jobConfig, stashBranches) {
+	if s.shouldDeleteJob(jobSummary, stashBranches) {
 		t.Fatalf("Want false\n")
 	}
 }
 
 func TestJobObsolete(t *testing.T) {
-	builtBranches := []jenkins.Branch{
-		jenkins.Branch{Name: "origin/feature/1"},
-	}
-	jobConfig := jenkins.JobConfig{SCM: jenkins.Scm{Branches: jenkins.Branches{Branch: builtBranches}}}
+	s := StatelessOperations{}
+	jobSummary := jenkins.JobSummary{Branch: "origin/feature/1"}
 	stashBranches := map[string]stash.Branch{"feature/2": stash.Branch{}}
-	if !shouldDeleteJob(jobConfig, stashBranches) {
+	if !s.shouldDeleteJob(jobSummary, stashBranches) {
 		t.Fatalf("Want false\n")
 	}
 }
-
-func TestJobBuildsMultipleBranches(t *testing.T) {
-	builtBranches := []jenkins.Branch{
-		jenkins.Branch{},
-		jenkins.Branch{},
-	}
-	jobConfig := jenkins.JobConfig{SCM: jenkins.Scm{Branches: jenkins.Branches{Branch: builtBranches}}}
-	stashBranches := map[string]stash.Branch{}
-	if shouldDeleteJob(jobConfig, stashBranches) {
-		t.Fatalf("Want false\n")
-	}
-}
-*/
