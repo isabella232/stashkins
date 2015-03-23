b9546d2770286c09ecf1a3e5672727dcd4b30fcc
Mark Petrovic <mark.petrovic@xoom.com>
2015-03-23 07:00:06 -0700

    test jenkins job type from xml

---
+++ stashkins/templateType_test.go
@@ -0,0 +1,36 @@
+package stashkins
+
+import (
+	"testing"
+
+	"github.com/xoom/jenkins"
+)
+
+var maven string = `<?xml version='1.0' encoding='UTF-8'?>
+<maven2-moduleset plugin="maven-plugin@2.7.1">
+    <description>Build maven</description>
+    </maven2-moduleset>`
+
+var freestyle string = `<?xml version='1.0' encoding='UTF-8'?>
+<project>
+  <description>Builds freestyle</description>
+</project>`
+
+var unknown string = `<?xml version='1.0' encoding='UTF-8'?>
+<nope>
+  <description>Builds freestyle</description>
+</nope>`
+
+func TestDocumentType(t *testing.T) {
+	if jobType, _ := templateType([]byte(maven)); jobType != jenkins.Maven {
+		t.Fatalf("Want jenkins.Maven type but got %v\n", jobType)
+	}
+
+	if jobType, _ := templateType([]byte(freestyle)); jobType != jenkins.Freestyle {
+		t.Fatalf("Want jenkins.Freestyle type but got %v\n", jobType)
+	}
+
+	if jobType, _ := templateType([]byte(unknown)); jobType != jenkins.Unknown {
+		t.Fatalf("Want jenkins.Unknown type but got %v\n", jobType)
+	}
+}
