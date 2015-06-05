f69871a5e77333a7753ec5a9c247764683bc385f
Mark Petrovic <mark.petrovic@xoom.com>
2015-06-05 10:18:49 -0700

    inf-2354 refactor get-templates to use tracking maps for continuous and release templates

--- stashkins/jobtemplate.go
+++ stashkins/jobtemplate.go
@@ -37,6 +37,7 @@ func templateType(xmlDocument []byte) (jenkins.JobType, error) {
 	return jenkins.Unknown, nil
 }
 
+// templateWalker returns a filepath.Walker that finds files named fileName.  Found files are returned in the input string array.
 func templateWalker(fileName string, found *[]string) filepath.WalkFunc {
 	return func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -49,8 +50,62 @@ func templateWalker(fileName string, found *[]string) filepath.WalkFunc {
 	}
 }
 
+// templateKey returns a key that indexes a map entry holding a JobTemplate pointer.
 func templateKey(project, slug string, projectType jenkins.JobType) string {
 	return fmt.Sprintf("%s.%s.%d", project, slug, projectType)
+}
+
+// projectCoordinates returns ("proj", "slug", nil) For input "/prefix/proj/slug/c.xml".  If the input is too short
+// to encompass proj/slug/c.xml an error is returned.  IF the input does not end in .xml an error is returned.
+func projectCoordinates(fullPath string) (string, string, error) {
+	s := strings.ToLower(fullPath)
+	if strings.HasPrefix(s, "/") {
+		s = s[1:]
+	}
+	parts := strings.Split(s, "/")
+
+	if len(parts) < 3 {
+		return "", "", fmt.Errorf("stashkins.GetTemplates Skipping invalid template repository record (Unexpected filesystem layout): %s\n", fullPath)
+	}
+	if !strings.HasSuffix(parts[len(parts)-1], ".xml") {
+		return "", "", fmt.Errorf("stashkins.GetTemplates Skipping invalid template file not ending in .xml: %s\n", fullPath)
+	}
+
+	return parts[len(parts)-3], parts[len(parts)-2], nil
+}
+
+// buildTemplates iterates over the input file set and builds a map of pointers to JobTemplates.  We need pointer to a JobTemplate so we can mutate it
+// when augmenting found continuous templates with associated release template data.
+func buildTemplates(files []string, f func(projectKey, slug string, data []byte, jobType jenkins.JobType) *JobTemplate) map[string]*JobTemplate {
+	templates := make(map[string]*JobTemplate)
+
+	for _, file := range files {
+		projectKey, slug, err := projectCoordinates(file)
+		if err != nil {
+			Log.Printf("%v\n", err)
+			continue
+		}
+
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			Log.Printf("stashkins.GetTemplates Skipping template repository record (Read template file) %s: %v\n", file, err)
+			continue
+		}
+
+		jobType, err := templateType(data)
+		if err != nil {
+			Log.Printf("stashkins.GetTemplates Skipping template repository record %s: %v\n", file, err)
+			continue
+		} else {
+			if jobType == jenkins.Unknown {
+				Log.Printf("stashkins.GetTemplates Skipping template repository record (unknown type)  %d: %v\n", file, jobType)
+				continue
+			}
+		}
+
+		templates[templateKey(projectKey, slug, jobType)] = f(projectKey, slug, data, jobType)
+	}
+	return templates
 }
 
 func GetTemplates(templateRepositoryURL, branch, cloneIntoDir string) ([]JobTemplate, error) {
@@ -69,89 +124,33 @@ func GetTemplates(templateRepositoryURL, branch, cloneIntoDir string) ([]JobTemp
 	}
 
 	// A temporary auditing map to track continuous templates.
-	continuousTemplateTracker := make(map[string]JobTemplate)
-
-	for _, file := range continuousTemplateFiles {
-		truncatedPath := strings.Replace(file, cloneIntoDir+"/", "", 1)
-		parts := strings.Split(truncatedPath, "/")
-		if len(parts) != 3 {
-			Log.Printf("stashkins.GetTemplates Skipping invalid template repository record (Unexpected filesystem layout): %s\n", truncatedPath)
-			continue
-		}
-		projectKey := strings.ToLower(parts[0])
-		slug := strings.ToLower(parts[1])
-
-		data, err := ioutil.ReadFile(file)
-		if err != nil {
-			Log.Printf("stashkins.GetTemplates Skipping template repository record (Read template file) %s: %v\n", file, err)
-			continue
-		}
-
-		jobType, err := templateType(data)
-		if err != nil {
-			Log.Printf("stashkins.GetTemplates Skipping template repository record %s: %v\n", file, err)
-			continue
-		} else {
-			if jobType == jenkins.Unknown {
-				Log.Printf("stashkins.GetTemplates Skipping template repository record (unknown type)  %d: %v\n", file, jobType)
-				continue
-			}
-		}
-
-		continuousTemplateTracker[templateKey(projectKey, slug, jobType)] = JobTemplate{ProjectKey: projectKey, Slug: slug, ContinuousJobTemplate: data, JobType: jobType}
-	}
+	continuousTemplates := buildTemplates(continuousTemplateFiles, func(projectKey, slug string, data []byte, jobType jenkins.JobType) *JobTemplate {
+		return &JobTemplate{ProjectKey: projectKey, Slug: slug, ContinuousJobTemplate: data, JobType: jobType}
+	})
 
 	// A temporary auditing map to track release templates.
-	releaseTemplateTracker := make(map[string]JobTemplate)
+	releaseTemplates := buildTemplates(continuousTemplateFiles, func(projectKey, slug string, data []byte, jobType jenkins.JobType) *JobTemplate {
+		return &JobTemplate{ProjectKey: projectKey, Slug: slug, ReleaseJobTemplate: data, JobType: jobType}
+	})
 
-	for _, file := range releaseTemplateFiles {
-		truncatedPath := strings.Replace(file, cloneIntoDir+"/", "", 1)
-		parts := strings.Split(truncatedPath, "/")
-		if len(parts) != 3 {
-			Log.Printf("stashkins.GetTemplates Skipping invalid template repository record (Unexpected filesystem layout): %s\n", truncatedPath)
-			continue
-		}
-		projectKey := strings.ToLower(parts[0])
-		slug := strings.ToLower(parts[1])
-
-		data, err := ioutil.ReadFile(file)
-		if err != nil {
-			Log.Printf("stashkins.GetTemplates Skipping template repository record (Read template file) %s: %v\n", file, err)
-			continue
-		}
-
-		jobType, err := templateType(data)
-		if err != nil {
-			Log.Printf("stashkins.GetTemplates Skipping template repository record %s: %v\n", file, err)
-			continue
-		} else {
-			if jobType == jenkins.Unknown {
-				Log.Printf("stashkins.GetTemplates Skipping template repository record (unknown type)  %d: %v\n", file, jobType)
-				continue
-			}
-		}
-
-		releaseTemplateTracker[templateKey(projectKey, slug, jobType)] = JobTemplate{ProjectKey: projectKey, Slug: slug, ReleaseJobTemplate: data, JobType: jobType}
-	}
-
-	// Augment an existing continuous template with release template data.  Mark this release template as processed by flagging it from the backing map.  This is safe.
-	for key, releaseTemplate := range releaseTemplateTracker {
-		if continuousTemplate, present := continuousTemplateTracker[key]; present {
+	// Augment existing continuous templates with release templates.  Mark this release template as processed by flagging it in the backing map --- this is safe.
+	for key, releaseTemplate := range releaseTemplates {
+		if continuousTemplate, present := continuousTemplates[key]; present {
 			continuousTemplate.ReleaseJobTemplate = releaseTemplate.ReleaseJobTemplate
-			delete(releaseTemplateTracker, key)
+			delete(releaseTemplates, key)
 		}
 	}
 
 	templates := make([]JobTemplate, 0)
 
 	// Add continuous templates to result
-	for _, v := range continuousTemplateTracker {
-		templates = append(templates, v)
+	for _, template := range continuousTemplates {
+		templates = append(templates, *template)
 	}
 
-	// Add release templates not associated with an existing continuous template
-	for _, v := range releaseTemplateTracker {
-		templates = append(templates, v)
+	// Add release templates not associated with an existing continuous template.  This would be an odd, but possible, condition.
+	for _, template := range releaseTemplates {
+		templates = append(templates, *template)
 	}
 
 	return templates, nil
--- stashkins/jobtemplate_test.go
+++ stashkins/jobtemplate_test.go
@@ -1,7 +1,13 @@
 package stashkins
 
 import (
+	"archive/zip"
+	"github.com/xoom/jenkins"
+	"io"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 )
@@ -68,4 +74,127 @@ func TestTrackingKey(t *testing.T) {
 	if s != "a.b.1" {
 		t.Fatalf("Want a.b.1 but got %s\n", s)
 	}
+}
+
+func TestProjectCoordinates(t *testing.T) {
+	proj, slug, err := projectCoordinates("/a/b/c.xml")
+	if err != nil {
+		t.Fatalf("Unexpected error: %v\n", err)
+	}
+	if proj != "a" {
+		t.Fatalf("Want a but got %s\n", proj)
+	}
+	if slug != "b" {
+		t.Fatalf("Want b but got %s\n", slug)
+	}
+
+	proj, slug, err = projectCoordinates("a/b/c.xml")
+	if err != nil {
+		t.Fatalf("Unexpected error: %v\n", err)
+	}
+	if proj != "a" {
+		t.Fatalf("Want a but got %s\n", proj)
+	}
+	if slug != "b" {
+		t.Fatalf("Want b but got %s\n", slug)
+	}
+
+	_, _, err = projectCoordinates("/a/b")
+	if err == nil {
+		t.Fatalf("Expected error for lack of file system parts\n")
+	}
+
+	_, _, err = projectCoordinates("/a/b/c")
+	if err == nil {
+		t.Fatalf("Expected error for file not ending in .xml\n")
+	}
+}
+
+func TestBuildTemplates(t *testing.T) {
+	sourceRepoDirectory, err := extractTestTemplates()
+	if err != nil {
+		t.Fatalf("Unexpected error extracting source templates: %v\n", err)
+	}
+
+	cloneDirectory, err := ioutil.TempDir("", "git-")
+	if err != nil {
+		t.Fatalf("Unexpected error creating temp dir: %v\n", err)
+	}
+
+	templates, err := GetTemplates("file://"+sourceRepoDirectory, "master", cloneDirectory)
+	if err != nil {
+		os.RemoveAll(cloneDirectory)
+		os.RemoveAll(sourceRepoDirectory)
+		t.Fatalf("Unexpected error: %v\n", err)
+	}
+
+	if len(templates) != 2 {
+		t.Fatalf("Want 2 but got %d\n", len(templates))
+	}
+	for _, template := range templates {
+		if template.ProjectKey != "playg1" && template.ProjectKey != "playg2" {
+			t.Fatalf("Want playg1 or playg2 but got %s\n", template.ProjectKey)
+		}
+		if template.Slug != "microservice" && template.Slug != "android" {
+			t.Fatalf("Want microservice or android but got %s\n", template.Slug)
+		}
+		if template.Slug == "microservice" && template.JobType != jenkins.Maven {
+			t.Fatalf("Want maven type for microservice but got %s\n", template.JobType)
+		}
+		if template.Slug == "android" && template.JobType != jenkins.Freestyle {
+			t.Fatalf("Want freestyle type for android but got %s\n", template.JobType)
+		}
+
+		if template.ReleaseJobTemplate == nil {
+			t.Fatalf("Expecting a non nil release job template: %#v\n", template)
+		}
+		if template.ContinuousJobTemplate == nil {
+			t.Fatalf("Expecting a non nil continuous job template: %#v\n", template)
+		}
+	}
+
+	os.RemoveAll(cloneDirectory)
+	os.RemoveAll(sourceRepoDirectory)
+}
+
+func extractTestTemplates() (string, error) {
+	r, err := zip.OpenReader("test-templates-git.zip")
+	if err != nil {
+		return "", err
+	}
+	defer r.Close()
+
+	name, err := ioutil.TempDir("", "templates-")
+	if err != nil {
+		return "", err
+	}
+
+	for _, f := range r.File {
+		if strings.HasSuffix(f.Name, "/") {
+			continue
+		}
+
+		destinationFileName := name + "/" + f.Name
+		parentDir := filepath.Dir(destinationFileName)
+		if err := os.MkdirAll(parentDir, 0755); err != nil {
+			return "", err
+		}
+
+		dst, err := os.Create(destinationFileName)
+		if err != nil {
+			return "", err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return "", err
+		}
+		_, err = io.Copy(dst, rc)
+		if err != nil {
+			return "", err
+		}
+		rc.Close()
+		dst.Close()
+	}
+	return name, nil
 }
